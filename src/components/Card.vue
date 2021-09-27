50aa7322fad451b825928668c922b7b4c2668bc7
Pietro <pietrospitella@gmail.com>
2021-09-28 00:41:40 +0200

    scaffolding

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h4>card</h4>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+      <Card/>
+  </main>
+</template>
+
+<script>
+import Card from './Card.vue'
+
+export default {
+name: 'Main',
+  components: {
+    Card,
+  }
+}
+</script>
+
+<style>
+
+</style>
